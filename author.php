615e0a3af0d23993a1669d77006f0b75c0e8c7e1
kunegun <kunegun@gmail.com>
2017-04-10 12:44:47 +0200

    ready

--- author.php
+++ author.php
@@ -6,7 +6,7 @@
 
 		<?php if (have_posts()): the_post(); ?>
 
-			<h1 class="single__title"><?php _e( 'Author Archives for ', 'html5blank' ); echo get_the_author(); ?></h1>
+			<h1 class="single__title"><?php _e( 'Author Archives for  ', 'html5blank' ); echo get_the_author(); ?></h1>
 
 		<?php if ( get_the_author_meta('description')) : ?>
 
@@ -21,29 +21,39 @@
 		<?php rewind_posts(); while (have_posts()) : the_post(); ?>
 
 			<!-- article -->
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<article id="post-<?php the_ID(); ?>" <?php post_class('noticias__item clear'); ?>>
 
 				<!-- post thumbnail -->
-				<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-						<?php the_post_thumbnail(array(120,120)); // Declare pixel size you need inside the array ?>
-					</a>
-				<?php endif; ?>
+				<div class="grid1-3">
+					<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
+						<div class="noticias__item__foto">
+							<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+								<?php the_post_thumbnail('mediano'); // Declare pixel size you need inside the array ?>
+							</a>
+						</div>
+					<?php endif; ?>
+				</div>
 				<!-- /post thumbnail -->
 
-				<!-- post title -->
-				<h2>
-					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-				</h2>
-				<!-- /Post title -->
+				<div class="grid2-3">
+					<!-- post title -->
+					<h2 class="noticias__item__title">
+						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+					</h2>
+					<!-- /Post title -->
 
-				<!-- post details -->
-				<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-				<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-				<span class="comments"><?php comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-				<!-- /post details -->
+					<!-- post details -->
+					<div class="noticias__item__detalles">
+						<span class="date"><?php the_date('d/m/Y'); ?> </span>
+						<!--span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span-->
+						<!--span class="comments"><?php comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span-->
+					</div>
+					<!-- /post details -->
+					<p class="noticias__item__text">
+						<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+					</p>
+				</div>
 
-				<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
 
 				<br class="clear">
 
@@ -72,6 +82,6 @@
 		<!-- /section -->
 	</main>
 
-<?php get_sidebar(); ?>
+<?php //get_sidebar(); ?>
 
 <?php get_footer(); ?>
--- loop.php
+++ loop.php
@@ -1,29 +1,39 @@
 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
 	<!-- article -->
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<article id="post-<?php the_ID(); ?>" <?php post_class('noticias__item clear'); ?>>
 
 		<!-- post thumbnail -->
-		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-				<?php the_post_thumbnail(array(120,120)); // Declare pixel size you need inside the array ?>
-			</a>
-		<?php endif; ?>
+		<div class="grid1-3">
+			<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
+				<div class="noticias__item__foto">
+					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+						<?php the_post_thumbnail('mediano'); // Declare pixel size you need inside the array ?>
+					</a>
+				</div>
+			<?php endif; ?>
+		</div>
+		<!-- /post thumbnail -->
 		<!-- /post thumbnail -->
 
-		<!-- post title -->
-		<h2>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-		</h2>
-		<!-- /post title -->
+		<div class="grid2-3">
+			<!-- post title -->
+			<h2 class="noticias__item__title">
+				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+			</h2>
+			<!-- /Post title -->
 
-		<!-- post details -->
-		<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-		<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-		<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-		<!-- /post details -->
-
-		<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+			<!-- post details -->
+			<div class="noticias__item__detalles">
+				<span class="date"><?php the_date('d/m/Y'); ?> </span>
+				<!--span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span-->
+				<!--span class="comments"><?php comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span-->
+			</div>
+			<!-- /post details -->
+			<p class="noticias__item__text">
+				<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+			</p>
+		</div>
 
 		<?php edit_post_link(); ?>
 
